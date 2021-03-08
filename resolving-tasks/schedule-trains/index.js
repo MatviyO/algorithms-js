4d087ff67720943a7767959713ed336cca1e92f6
Oleh <oleh.m97@gmail.com>
2021-03-08 19:56:17 +0200

    up

---
+++ resolving-tasks/schedule-trains/index.js
@@ -0,0 +1,29 @@
+/*
+    13:00 A 15:30 B
+    12:00 C 16:00 D
+    10:00 B 11:00 C
+    10:00 A 11:00 C
+*/
+
+
+const shedule = [
+    ['13:00', 'A', '15:30', 'B'],
+    ['12:00', 'C', '16:00', 'D'],
+    ['10:00', 'B', '11:00', 'C'],
+    ['10:00', 'A', '11:00', 'C']
+]
+
+
+
+//function convert
+
+function convert() {
+
+}
+
+
+
+
+
+
+// watch?v=jsF8WcLKRww&ab_channel=DmitriyKalyuk
