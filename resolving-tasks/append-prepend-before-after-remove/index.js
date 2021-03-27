26c33c63e329b3805b332cc4c0615bd36ff34655
Oleh <oleh.m97@gmail.com>
2021-03-27 12:45:37 +0200

    up

--- resolving-tasks/append-prepend-before-after-remove/index.js
+++ resolving-tasks/append-prepend-before-after-remove/index.js
@@ -13,3 +13,37 @@ div1.textContent = 'New Prepend'
 div1.classList.add('bg-primary')
 
 logBlock.prepend(div1); // prepend this method  added to start in element
+
+//before
+let div2 = document.createElement('div')
+
+div2.textContent = 'New before'
+div2.classList.add('bg-primary')
+
+logBlock.before(div2); // before this method  added pered eleventom top  1 lvl
+
+//after
+let div3 = document.createElement('div')
+
+div3.textContent = 'New after'
+div3.classList.add('bg-primary')
+
+logBlock.after(div3); // after this method  added pered eleventom top  1 lvl
+
+
+//replaceWith
+let div4 = document.createElement('div')
+
+div4.textContent = 'New replaceWith'
+div4.classList.add('bg-primary')
+
+logBlock.replaceWith(div4); // replace old div and add new div
+
+
+//remove
+let div4 = document.createElement('div')
+
+div4.textContent = 'New remove'
+div4.classList.add('bg-primary')
+
+logBlock.remove(div4); // remove div
