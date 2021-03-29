05e7e6e24789cfc846309ce8406d20025f46d97d
Oleh <oleh.m97@gmail.com>
2021-03-29 22:17:24 +0300

    up

--- resolving-tasks/append-prepend-before-after-remove/index.js
+++ resolving-tasks/append-prepend-before-after-remove/index.js
@@ -1,11 +1,10 @@
 const logBlock = document.querySelector('.log-block')
-
 let div = document.createElement('div')
 
 div.textContent = 'New Div'
 div.classList.add('bg-primary')
-
 logBlock.append(div); // append   this method  added to end in element
+
 
 let div1 = document.createElement('div')
 
@@ -13,6 +12,7 @@ div1.textContent = 'New Prepend'
 div1.classList.add('bg-primary')
 
 logBlock.prepend(div1); // prepend this method  added to start in element
+
 
 //before
 let div2 = document.createElement('div')
