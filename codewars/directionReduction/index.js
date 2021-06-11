4cfffffa1503aa42cfdde7541033ac825b773396
Oleh <oleh.m97@gmail.com>
2021-06-11 21:46:33 +0300

    reduction

---
+++ codewars/directionReduction/index.js
@@ -0,0 +1,16 @@
+function directionReduction(arr) {
+    const oppositeDirs = {
+        NORTH: 'SOUTH',
+        SOUTH: 'NORTH',
+        WEST: 'EAST',
+        EAST: 'WEST'
+    }
+    return arr.reduce((acc, el) => {
+        if (acc[acc.length -1] === oppositeDirs[el]) {
+            acc.pop()
+        } else {
+            acc.push(el)
+        }
+        return acc;
+    }, [])
+}
