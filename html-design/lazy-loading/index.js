d58bcf55577d1fbb1d370b1a6b88f0467da23b35
Oleh <oleh.m97@gmail.com>
2021-06-28 18:51:10 +0300

    ds

--- html-design/lazy-loading/index.js
+++ html-design/lazy-loading/index.js
@@ -1,7 +1,7 @@
 const lazyImages = document.querySelectorAll('img[data-src], source[data-srcset]');
 const loadMapBlock = document.querySelector('.load-map');
 const windowHeight = document.documentElement.clientHeight;
-
+const loadMoreBlock = document.querySelectorAll('._loadMore')
 
 let lazyImagesyPosition = [];
 if ( lazyImages.length > 0) {
@@ -13,11 +13,27 @@ if ( lazyImages.length > 0) {
     })
 }
 
+
+window.addEventListener('scroll', lazyScroll)
+
+function lazyScroll() {
+    if(document.querySelectorAll('img[data-src], source[data-srcset]').length > 0) {
+        lazyScrollCheck()
+    }
+    if (!loadMapBlock.classList.contains('_loaded')) {
+        getMap()
+    }
+
+    if (!loadMoreBlock.classList.contains('_loading')) {
+        loadMore()
+    }
+}
+
+
 function lazyScrollCheck() {
     let imgIndex = lazyImagesyPosition.findIndex(
         item => pageYOffset > item - windowHeight
     );
-
     if ( imgIndex >= 0) {
         if(lazyImages[imgIndex].dataset.src) {
             lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
@@ -26,9 +42,49 @@ function lazyScrollCheck() {
             lazyImages[imgIndex].srcset = lazyImages[imgIndex].dataset.srcset;
             lazyImages[imgIndex].removeAttribute('data-srcset')
         }
-
         delete lazyImagesyPosition[imgIndex];
-
     }
 
+}
+
+function getMap() {
+    const loadMapBlockPosition = loadMapBlock.getBoundingClientRect().top + pageYOffset;
+    if (pageYOffset > loadMapBlockPosition - windowHeight ) {
+        const loadMapUrl = loadMapBlock.dataset.map;
+        if (loadMapUrl) {
+            loadMapBlock.insertAdjacentElement("beforeend", `<iframe src="${loadMapUrl}" style="border: 0;" allowfullscreen="" loading="lazy" ></iframe>`)
+        }
+        loadMapBlock.classList.add('_loaded')
+    }
+}
+
+
+function loadMore() {
+    const loadMoreBlockPosition = loadMoreBlock.getBoundingClientRect().top + pageYOffset;
+    const loadMoreHeight =  loadMoreBlock.offsetHeight;
+
+    if(pageYOffset >(loadMoreBlockPosition + loadMoreHeight) - windowHeight) {
+        getContent()
+    }
+
+}
+
+async function getContent() {
+    if (!document.querySelector('._loadingicon')) {
+        loadMoreBlock.insertAdjacentElement('beforeend', `<div class="_loading-icon"></div>`)
+    }
+
+    loadMoreBlock.classList.add('_loading')
+
+    let response = await fetch('_more.html', {method: 'GET'})
+    if(response.ok) {
+        let result = await response.text()
+        loadMoreBlock.insertAdjacentElement('beforeend', result);
+        loadMoreBlock.classList.remove('_loading')
+    }
+    if (document.querySelector('._loading-icon')) {
+        document.querySelector('._loading-icon').remove();
+    } else {
+        alert('Error')
+    }
 }
