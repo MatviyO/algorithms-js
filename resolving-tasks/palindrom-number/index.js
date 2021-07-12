ff23dd0d166c4099bbad33e2cd3d767435010e84
Oleh <oleh.m97@gmail.com>
2021-07-12 23:39:44 +0300

    fdg

---
+++ resolving-tasks/palindrom-number/index.js
@@ -0,0 +1,12 @@
+const input1 = 121;
+const input2 = -121;
+const input3 = 10;
+
+const isPalindromeNumber = function (x) {
+    if (x < 0) return false;
+    
+}
+
+console.log(isPalindromeNumber(input1))
+console.log(isPalindromeNumber(input2))
+console.log(isPalindromeNumber(input3))
