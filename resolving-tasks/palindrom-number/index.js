c433a50c631582f48f8bd1a14b6a89fe44eb56bd
Oleh <oleh.m97@gmail.com>
2021-07-12 23:53:35 +0300

    dasdas

--- resolving-tasks/palindrom-number/index.js
+++ resolving-tasks/palindrom-number/index.js
@@ -4,7 +4,19 @@ const input3 = 10;
 
 const isPalindromeNumber = function (x) {
     if (x < 0) return false;
-    
+    if (x % 10 === 0)  return false;
+    if (x < 10)  return true;
+
+    let rev = 0;
+    while ( x > rev) {
+        rev *= 10;
+        rev += x % 10;
+        x = Math.trunc(x/10);
+
+    }
+
+    return x === rev || x === Math.trunc(rev/10);
+
 }
 
 console.log(isPalindromeNumber(input1))
