bcf1f8b2753bc8bec227d9b59be5fdeb68fd7a6a
Oleh <oleh.m97@gmail.com>
2021-07-31 22:02:08 +0300

    gdfgdf

--- resolving-tasks/true {}/index.js
+++ resolving-tasks/true {}/index.js
@@ -1,34 +1,27 @@
-function findSubstring(s, k) {
-    const result = {
-        str: 'Not found',
-        rating: 0
+let str = '([)['
+
+function isValid(s) {
+    let stack = [];
+    let brackets = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
     }
 
-    const d = ['a', 'e', 'i', 'o', 'u']
+    for (let i = 0; i < s.length; i++) {
+        const current = s[i];
+        if (isClodesBracket(current)) {
+            if (brackets[current] !== stack.pop()) return false
 
-    let rating = 0;
-    let str = '';
+        } else {
+            stack.push(current)
+        }
 
-    for(let i =0; i <s.length; i ++) {
-        if (str.length >= k) {
-            if (d.includes(str[0])) {
-                rating--;
-            }
-            str = str.substr(1, str.length)
-        }
-        str += s[i];
-        if (d.includes(s[i])) {
-            rating++;
-        }
-        if (rating > result.rating) {
-            result.rating = rating;
-            result.str = str;
-        }
-        return result.str;
     }
 
+    return stack.length === 0;
 }
 
-console.log('2354364564535464', '=====>', findSubstring('23443534534534', 6), '===', 'Not Found')
-console.log('caberqiitefg','=====>', findSubstring('caberqiitefg', 5), '===', 'erqii')
-console.log('aeiouia','=====>', findSubstring('aeiouia', 3), '===', 'aei')
+function isClodesBracket(ch) {
+    return [')', '}', ']'].indexOf(ch) > -1;
+}
