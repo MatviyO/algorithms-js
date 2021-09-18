a6cb4e957cec32fc34092eafb9cb97dbf72813c6
Oleh <oleh.m97@gmail.com>
2021-09-18 22:57:47 +0300

    Update index.js

--- codewars/waterContainer/index.js
+++ codewars/waterContainer/index.js
@@ -1,7 +1,7 @@
-const input1 = [1,8,6,2,5,4,8,3,7]
-const input1 = [1,1]
-const input1 = [4,3,2,1,4]
-const input1 = [1,2,1]
+const input1 = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+const input2 = [1, 1]
+const input3 = [4, 3, 2, 1, 4]
+const input4 = [1, 2, 1]
 
 function maxArea(height) {
     let maxArea = 0;
@@ -11,14 +11,31 @@ function maxArea(height) {
         let currentVolume = Math.min(height[left], height[right]) * (right - left)
         maxArea = Math.max(maxArea, currentVolume);
         if (height[left] < height[right]) {
-               left++ 
+            left++
         } else {
             right -= 1;
         }
     }
 
     return maxArea;
-    
+
 }
 
-console.log(input1, maxArea(input1))
+// console.log(input1, maxArea(input1))
+
+
+function sayHi(person) {
+    const name = person.name;
+    setTimeout(() => {
+        console.log('Hello, ' + name);
+    }, 3000);
+}
+
+let someone = { name: 'Dan' };
+sayHi(someone);
+
+someone = { name: 'Yuzhi' };
+sayHi(someone);
+
+someone = { name: 'Dominic' };
+sayHi(someone);
