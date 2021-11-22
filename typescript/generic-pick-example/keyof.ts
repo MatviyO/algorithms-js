1f3145413beeec71af2439d5b71127a771e3396b
Oleh <oleh.m97@gmail.com>
2021-11-22 22:52:09 +0200

    up

--- typescript/generic-pick-example/keyof.ts
+++ typescript/generic-pick-example/keyof.ts
@@ -8,3 +8,26 @@ type MyColor = keyof typeof Color;
 const one: MyColor = 'white'
 console.log(one)
 console.log(typeof door)
+
+//
+
+const formData = {
+    name: 'Jon',
+    age: 30
+}
+type ValidationResult = {
+    [key in keyof typeof  formData]: boolean
+}
+
+// declare function validate<T>(value: T): ValidationResult;
+declare function validate<T>(value: T): { [key in keyof T]: boolean};
+
+const r = validate<typeof formData>(formData)
+console.log(r)
+
+
+type myType = {
+    age: string
+    name: string
+}
+const v: myType = {name: '', age: ''}
