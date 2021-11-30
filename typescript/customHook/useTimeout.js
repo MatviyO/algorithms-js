0860115e5e73d54bbb7b866756028c6089fc05b7
Oleh <oleh.m97@gmail.com>
2021-11-30 21:51:37 +0200

    ензу

--- typescript/customHook/useTimeout.js
+++ typescript/customHook/useTimeout.js
@@ -2,6 +2,8 @@ export default function useTimeout(callback, delay) {
     const callbackRef = useRef(callback)
     const timeouRef = useRef()
 
+    
+
     useEffect(() => {
         callbackRef.current = callback
     }, [callback])
