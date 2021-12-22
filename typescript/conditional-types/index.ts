f471cbec8b408872f9bc4f93bd22a74edad497fc
Oleh <oleh.m97@gmail.com>
2021-12-22 19:53:47 +0200

    init

--- typescript/conditional-types/index.ts
+++ typescript/conditional-types/index.ts
@@ -11,3 +11,23 @@ function getId<T extends { id: any}>(obj: T): T extends { id: string} ? string :
 
 const r1 = getId({} as User)
 const r2 = getId({} as Message)
+
+
+//example 2
+
+type NotFalsy<T = null> = T extends (null | undefined | false | 0) ? never : T;
+
+let z: NotFalsy;
+z = 'jello'  //error
+let z1: NotFalsy<string>
+
+z1 = 'string'
+z1 = null // error 
+
+//example 3
+
+type Filter<T, U> = T extends U ? never : T;
+type R = Filter<'a' | 'b' | 'c', 'b'>  // b removed , because b === b  was true and equal never ( never skip it)
+
+// also 
+type R1 = Exclude<'a' | 'b' | 'c', 'b'>
