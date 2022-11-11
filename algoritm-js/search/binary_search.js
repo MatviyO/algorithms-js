042920bf1c7c68dfee63c5b0556f38e02035540d
Oleh <oleh.m97@gmail.com>
2022-11-11 23:04:20 +0200

    binary search

--- algoritm-js/search/binary_search.js
+++ algoritm-js/search/binary_search.js
@@ -1,15 +1,20 @@
-const arr = [1,2,3,4,5,6,7,8,9];
+const arr = [1,2,3,4,5,6,7,8,9, 10, 11, 12, 13, 14];
+let count1 = 0;
+let count2 = 0;
 
+//fast after binary recursion ,  2 rating number
 function binarySearch(array, item) {
     let start = 0;
     let end = array.length
+    let mid;
     let found = false;
     let position = -1;
-    while (!found && start <= end) {
-        let mid = Math.floor(start + end / 2)
+    while (found === false && start <= end) {
+        count1++;
+         mid = Math.floor((start + end )/ 2)
         if (array[mid] === item) {
             found = true;
-            let position = mid;
+            position = mid;
             return position;
         }
         if (array[mid] > item) {
@@ -18,6 +23,22 @@ function binarySearch(array, item) {
             start = mid + 1;
         }
     }
+    return position
+}
+console.log(binarySearch(arr, 10));
+console.log(count1);
+
+//example 2,  binary search with recursion , best fast , 1 rating number
+function binarySearchRecursion(array, item, start, end) {
+    let middle = Math.floor((start + end) / 2);
+    if (item === array[middle]) return middle;
+    count2++;
+    if (item < array[middle]) {
+        return binarySearchRecursion(array, item, start, middle - 1)
+    } else {
+        return binarySearchRecursion(array, item, middle + 1, end)
+    }
 }
 
-console.log(binarySearch(arr));
+console.log(binarySearchRecursion(arr, 10, 0 ,arr.length));
+console.log(count2);
--- algoritm-js/sorting/quick-sort.js
+++ algoritm-js/sorting/quick-sort.js
@@ -1,11 +1,26 @@
 //по принципу поділу, ділимо масив на підмасиви, ділимо по центру
+//cамий кращий
 const arr = [6,1,5,9,7,3,11,2];
 
+//example 1
 function quickSort(array) {
     if (array.length <= 1) {
         return array;
-
     }
+
+    let midIndex = Math.floor(array.length / 2);
+    let midValue = array[midIndex];
+    let less = [];
+    let more = [];
+    for (let i = 0; i < array.length; i++) {
+        if (i === midIndex) continue;
+        if (array[i] < midValue) {
+            less.push(array[i])
+        } else {
+            more.push(array[i])
+        }
+    }
+    return [...quickSort(less), midValue, ...quickSort(more)]
 }
 
 console.log(quickSort(arr))
